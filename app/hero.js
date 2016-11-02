708c208d161010184265fab799324aca03f0ff2a
hujingliang <1617451312@qq.com>
2016-11-02 14:06:15 +0800

    '组件式开发'

---
+++ app/hero.js
@@ -0,0 +1,11 @@
+"use strict";
+/**
+ * Created by jialao on 2016/11/2.
+ */
+var Hero = (function () {
+    function Hero() {
+    }
+    return Hero;
+}());
+exports.Hero = Hero;
+//# sourceMappingURL=hero.js.map
---
+++ app/hero.ts
@@ -0,0 +1,7 @@
+/**
+ * Created by jialao on 2016/11/2.
+ */
+export class Hero {
+    id: number;
+    name: string;
+}
