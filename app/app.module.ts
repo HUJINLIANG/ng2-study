b78c26837839360cd706a7516db2dfa31aa94de7
hujingliang <1617451312@qq.com>
2016-11-08 16:24:14 +0800

    'http接口'

--- app/app.module.ts
+++ app/app.module.ts
@@ -5,6 +5,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import {RouterModule} from '@angular/router';
 import {FormsModule} from '@angular/forms'
+import {HttpModule} from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { AppComponent }   from './app.component';
 import HeroService from './hero.service';
@@ -18,7 +22,10 @@ import {AppRoutingModule} from './app-routing.module';
     imports: [
         BrowserModule,
         FormsModule,
-        AppRoutingModule],
+        AppRoutingModule,
+        HttpModule,
+        InMemoryWebApiModule.forRoot(InMemoryDataService),
+    ],
     declarations: [ AppComponent,HeroDetailComponent,HeroesComponent,DashboardComponent ],
     providers:[HeroService],
     bootstrap:    [ AppComponent ]
--- app/hero.service.ts
+++ app/hero.service.ts
@@ -2,13 +2,27 @@
  * Created by jialao on 2016/11/3.
  */
 import {Injectable} from '@angular/core';
+import {Http,Headers} from '@angular/http'
+import 'rxjs/add/operator/toPromise';
+
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes'
 
 @Injectable()
 export default class HeroService{
+    private heroesUrl:string = 'app/heroes';
+    private handleError(error:any):Promise<any>{
+        console.err('error occured',error);
+        return Promise.reject(error.message || error);
+    }
+
+    constructor(private http:Http){ }
+
     getHeroes():Promise<Hero[]>{
-        return Promise.resolve(HEROES);
+        return this.http.get(this.heroesUrl)
+            .toPromise()
+            .then(response => {console.log(response);return response.json().data as Hero[];})
+            .catch(this.handleError);
     }
     getHeroesSlowly(): Promise<Hero[]> {
         return new Promise<Hero[]>(resolve =>
